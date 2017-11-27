5a46ed00cd21081613350224ae805a503693f359
Tyler Bennett <tbennett6421@gmail.com>
2017-11-26 20:58:35 -0600

    Creating new backup script, starting with header

---
+++ backup-scripts/rsync-running-config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+##
+## Author:        Tyler Bennett
+## Email:         tbennett6421@gmail.com
+## Date:          2017-11-26
+## Usage:         rsync-running-config.sh
+## Description:   Backup a system using duplicity
