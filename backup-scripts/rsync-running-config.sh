59d5bab17692e4c67aeb1186e3deea95e39b7fcb
Tyler Bennett <tbennett6421@gmail.com>
2017-11-26 21:42:56 -0600

    Added some tagging to the code as per refactor process

--- backup-scripts/rsync-running-config.sh
+++ backup-scripts/rsync-running-config.sh
@@ -5,6 +5,16 @@
 ## Date:          2017-11-26
 ## Usage:         rsync-running-config.sh
 ## Description:   Backup a system using duplicity
+##
+## Settings required to be user-set will be tagged as follows. ##!!
+## This allows required options to be "greppable" using fgrep '##!!'
+##			
+##
+##@@ During refactoring, options to be evaluated for removal
+##@@ i.e. not necessary, are marked as follows.
+##@@		##@@EXIT_ERR_GREP=4
+##@@
+
 BOOL_TRUE=1
 BOOL_FALSE=0
 
@@ -12,8 +22,8 @@ BOOL_FALSE=0
 ### Begin configurable options;
 ### set options using $BOOL_TRUE, $BOOL_FALSE, 'stringvalues' or integer
 ########################################################################
-SSH_PASSWORD='setme'
-GPG_PASSWORD='setme'
+SSH_PASSWORD='setme'   ##!!
+GPG_PASSWORD='setme'   ##!!
 LOG_DIR='/var/log/dr'
 B_DEBUG=$BOOL_TRUE          # should this program be verbose
 B_MAIL=$BOOL_TRUE           # should this program use email subsystem
