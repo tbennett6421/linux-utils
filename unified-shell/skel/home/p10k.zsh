b27260322ba4e92cc14a0491b9cb0ca1cbdc5a53
Tyler Bennett <tyler.bennett@availity.com>
2021-09-21 17:23:42 -0500

    more python info

--- unified-shell/skel/home/p10k.zsh
+++ unified-shell/skel/home/p10k.zsh
@@ -891,8 +891,10 @@
   ###[ virtualenv: python virtual environment (https://docs.python.org/3/library/venv.html) ]###
   # Python virtual environment color.
   typeset -g POWERLEVEL9K_VIRTUALENV_FOREGROUND=37
+  ### Indicate the venv we are in
+  typeset -g POWERLEVEL9K_VIRTUALENV_GENERIC_NAMES=()
   # Don't show Python version next to the virtual environment name.
-  typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=false
+  # typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=false
   # If set to "false", won't show virtualenv if pyenv is already shown.
   # If set to "if-different", won't show virtualenv if it's the same as pyenv.
   typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_WITH_PYENV=false
